6c01acc079cbc0b8a8a64a613968fc84cee7e2ad
MC <119978958+mcbariekman@users.noreply.github.com>
2023-01-17 23:29:11 -0800

    js edits 2

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,40 +1,22 @@
 // Assignment code here
-function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)=>
-{const availableChars =[
-  ...(includeUppercase ? uppercaseLetters: []),
-  ...(includeNumbers ? numbers: [])
-  ...(includeSymbols ? symbols: [])
-];
-}
+var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+var string_length = 8;
+var randomstring = Math.random().toString(36).slice(-8);
+const specialCharacters = "!@#$%^&*()";
 
-console.log(availableChars)
-
-generatePassword(12, true, true, true, true, true);
+console.log(Math.floor(5));
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
-console.log(generateBtn)
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  if(inputtxt.value.match(passw)) 
-  { 
-  alert('Correct, try another...')
-  return true;
-  }
-  else
-  { 
-  alert('Wrong...!')
-  return false;
-  }
   }
 
-console.log(passwordText)
-
-/* function display() {
-  var x = document.getElementById("myPsw").value;
+generateBtn.addEventListener("click", writePassword);
+console.log(generatePassword);
+function generatePassword() {
+  var passwordLength = prompt("Please enter a password");
 }
-*/ 
