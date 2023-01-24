efd24e35b9fbc0a07d0b8d0352736cc93bbe7421
MC <119978958+mcbariekman@users.noreply.github.com>
2023-01-23 17:30:30 -0800

    1-23-23

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,27 +1,36 @@
 // Assignment code here
-var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-var length = window.prompt("choose password length between 8 and 128")
-char = "abcdefghijklmnopqrstuvwkyz"
-num = "1234567890"
-passwordText = [""]
- for(i=0;i < length; i++){
-   passwordText += char.charAt(Math.floor(Math.random() * char.length))
-   return passwordText
- }
-};
-
-console.log(Math.floor(5)); 
-
+var length = window.prompt("choose password length between 8 and 128");
+const chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const num = "0123456789";
+const symbols = "!@#$%&'()*+,-./:;<=>?@";
+const passwordText = document.querySelector("#password");;
+const lengthText = document.querySelector("#length");
+const incNumbers = document.documentElementById("numbers");
+const incSymbols = document.documentElementById("symbols");
+const generateBtn = document.documentElementById("generate");
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-console.log(generateBtn)
+generateBtn.addEventListener("click", () => {
+  let characters = chars;
+  incNumbers.checked ? (characters += numbers) : "";
+  incSymbols.checked ? (characters += symbols) : "";
+  passwordTxt.value = generatePassword(length.value, characters);
+});
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+const generatePassword = (length, characters) => {
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    password += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
   }
+  return password;
+};
 
-  console.log(generatePassword)
+const copyBtn = document.getElementById("copy");
+copyBtn.addEventListener("click", () => {
+  passwordTxt.select();
+  document.commandId("copy");
+  alert("Password Copied");
+});
